7793d57f57a6c38fe6fe26edfe54bbf1efed01a5
kzycn <kzycn@qq.com>
2021-07-16 00:02:42 +0800

    测试又拍云文件上传

--- float-system/src/test/java/com/fsyume/FloatApplicationTests.java
+++ float-system/src/test/java/com/fsyume/FloatApplicationTests.java
@@ -1,13 +1,36 @@
 package com.fsyume;
 
+import com.upyun.RestManager;
+import com.upyun.UpYunUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FloatApplicationTests {
 
+    @Value("${upYun.upYunServerName}")
+    private String ServerName;
+
+    @Value("${upYun.upYunUserName}")
+    private String UserName;
+
+    @Value("${upYun.upYunPassword}")
+    private String password;
+
+    @Value("${upYun.upYunUrl}")
+    private String Url;
+
     @Test
-    void contextLoads() {
+    void upYunUpdate() {
+
+        RestManager manager = new RestManager(ServerName,UserName,password);
+
+
+
+        System.out.println(Url);
+
     }
 
 }
