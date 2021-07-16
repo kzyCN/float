c2a522e641d2de8b9514a633639e1fc298b411ff
kzycn <kzycn@qq.com>
2021-07-16 09:46:16 +0800

    前端架构更新

--- float-ui/src/App.vue
+++ float-ui/src/App.vue
@@ -1,34 +1,12 @@
 <template>
   <div id="app">
-    <el-input v-model="input" placeholder="请输入内容"></el-input>
-    <el-radio v-model="radio" label="1">备选项</el-radio>
-    <el-radio v-model="radio" label="2">备选项</el-radio>
-    <el-switch
-        v-model="value"
-        active-color="#13ce66"
-        inactive-color="#ff4949">
-    </el-switch>
-
-    <el-date-picker
-        v-model="value1"
-        type="datetime"
-        placeholder="选择日期时间">
-    </el-date-picker>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -37,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- float-ui/src/main.js
+++ float-ui/src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI,axios)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
