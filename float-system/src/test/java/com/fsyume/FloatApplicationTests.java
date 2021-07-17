01b95e118308a3bc1f48d71c6c5d3d57ce2cf254
kzycn <kzycn@qq.com>
2021-07-17 17:15:46 +0800

    新增又拍云上传工具

--- float-system/src/test/java/com/fsyume/FloatApplicationTests.java
+++ float-system/src/test/java/com/fsyume/FloatApplicationTests.java
@@ -1,17 +1,16 @@
 package com.fsyume;
 
-import com.upyun.RestManager;
-import com.upyun.UpException;
-import com.upyun.UpYunUtils;
+import com.upyun.*;
 import okhttp3.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,8 +20,8 @@ class FloatApplicationTests {
     @Value("${upYun.upYunServerName}")
     private String ServerName;
 
-    @Value("${upYun.upYunUserName}")
-    private String UserName;
+    @Value("${upYun.upYunUsername}")
+    private String Username;
 
     @Value("${upYun.upYunPassword}")
     private String password;
@@ -33,7 +32,7 @@ class FloatApplicationTests {
     @Test
     void upYunUpdate() {
 
-        RestManager manager = new RestManager(ServerName,UserName,password);
+        RestManager manager = new RestManager(ServerName, Username,password);
 
         // 设置超时时间
         manager.setTimeout(60);
@@ -53,6 +52,7 @@ class FloatApplicationTests {
         // 设置待上传文件的 Content-MD5 值
         // 如果又拍云服务端收到的文件MD5值与用户设置的不一致，将回报 406 NotAcceptable 错误
         File file = new File(str);
+
         params.put(RestManager.PARAMS.CONTENT_MD5.getValue(), UpYunUtils.md5(file, 1024));
 
         // 设置待上传文件的"访问密钥"
@@ -75,6 +75,39 @@ class FloatApplicationTests {
 
 
         System.out.println(Url);
+
+    }
+
+    @Test
+    void upYunFromUpLoader() throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+        FormUploader formUploader = new FormUploader(ServerName, Username, password);
+
+        //初始化参数组 Map
+        final Map<String, Object> paramsMap = new HashMap<String, Object>();
+
+        //添加 SAVE_KEY 参数
+        //filename为文件名(例如：12345.jpg)
+        String filename = "kx.png";
+        paramsMap.put(Params.SAVE_KEY, "/float/img/" + filename);
+
+        //添加同步上传作图参数 X_GMKERL_THUMB
+        // //限定图片宽度为 300px、锐化、压缩质量 80、存储为 png 格式（参数不区分先后顺序）
+        paramsMap.put(Params.X_GMKERL_THUMB, "/fw/300/unsharp/true/quality/80/format/png");
+
+        //打印结果
+        Result result = null;
+
+        String imgPath = "C:/Users/kzy/Pictures/kx.png";
+        File file = new File(imgPath);
+
+        result = formUploader.upload(paramsMap, file);
+
+        System.out.println(result);
+
+        String msg = result.getMsg();
+
+        System.out.println(msg);
+
 
     }
 
